fd0731b227b3e6e40567c816896d8e05a5bd6e4b
zlzcty <fake@exmaple.com>
2025-11-22 10:01:13 -0800

    fix bad downloads in libtiff
    may need to copy the same patches to elsewhere

--- magma/targets/libtiff/build.sh
+++ magma/targets/libtiff/build.sh
@@ -19,6 +19,20 @@ mkdir -p "$WORK"
 mkdir -p "$WORK/lib" "$WORK/include"
 
 cd "$TARGET/repo"
+
+mkdir -p config
+# Manually download these files from a mirror and replace the part of autogen.sh that downloads them
+echo "attempting to download from gnu.org"
+wget --tries=3 --timeout=15 -O "$(pwd)/config/config.guess" \
+    https://raw.githubusercontent.com/pabs3/gnuconfig/refs/heads/master/config.guess || exit 1
+wget --tries=3 --timeout=15 -O "$(pwd)/config/config.sub" \
+    https://raw.githubusercontent.com/pabs3/gnuconfig/refs/heads/master/config.sub || exit 1
+
+
+chmod a+x config/config.*
+sed -i '/^for file in config\.guess config\.sub/,/^done$/d' autogen.sh
+cat ./autogen.sh
+
 ./autogen.sh
 ./configure --disable-shared --prefix="$WORK"
 make -j$(nproc) clean
@@ -28,5 +42,5 @@ make install
 cp "$WORK/bin/tiffcp" "$OUT/"
 $CXX $CXXFLAGS -std=c++11 -I$WORK/include \
     contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc -o $OUT/tiff_read_rgba_fuzzer \
-    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic \
+    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lzstd -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic \
     $LDFLAGS $LIBS
